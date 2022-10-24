4142417c2e89eb2f246d3265ccd4bea02e069130
mrsagcan <mrsagcan@yahoo.com>
2022-10-24 03:17:31 +0300

    Index buffers for same vertexed triangles

--- lGl/Main.cpp
+++ lGl/Main.cpp
@@ -72,28 +72,43 @@ int main()
 
 	GLfloat vertices[] =
 	{
-		-0.5f, -0.5f, 
-		-0.5f, 0.5f, 
-		0.5f, 0.5f,
-		0.5f, -0.5f
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	};
+
+	GLuint indices[]
+	{
+		0, 3, 5, //Lower left triangle
+		3, 2, 4, //Lower right triangle
+		5, 4, 1 // Upper triangle
 	};
 
 
-	GLuint VAOs[1], VBOs[1];
 
-	glGenVertexArrays(1, VAOs);
-	glGenBuffers(1, VBOs);
+	GLuint VAO, VBO, EBO;
 
-	glBindVertexArray(VAOs[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
 
@@ -103,8 +118,8 @@ int main()
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAOs[0]);
-		glDrawArrays(GL_LINE_LOOP, 0, 4);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		// Take care of all GLFW events
@@ -115,8 +130,9 @@ int main()
 
 	//Delete and terminate GLFW before ending the program
 
-	glDeleteVertexArrays(1, VAOs);
-	glDeleteBuffers(1, VBOs);
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
