332b1061300e855fbdf0f563bb7ba64d26c69c1b
mrsagcan <mrsagcan@yahoo.com>
2022-10-24 03:23:30 +0300

    Squares with index buffers

--- lGl/Main.cpp
+++ lGl/Main.cpp
@@ -72,19 +72,16 @@ int main()
 
 	GLfloat vertices[] =
 	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+		-0.5f, -0.5f, 0.0f, //Lower left corner
+		-0.5f, 0.5f, 0.0f,  //Upper left corner
+		0.5f, -0.5f, 0.0f,	//Lower right corner
+		0.5f, 0.5f, 0.0f	//Upper right corner
 	};
 
 	GLuint indices[]
 	{
-		0, 3, 5, //Lower left triangle
-		3, 2, 4, //Lower right triangle
-		5, 4, 1 // Upper triangle
+		0, 1, 2, //Lower left triangle
+		1, 2, 3  //Upper right triangle
 	};
 
 
@@ -119,7 +116,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		// Take care of all GLFW events
