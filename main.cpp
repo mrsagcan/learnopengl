bf8c3e15e81f68f3c97aa8a4a452e857f41c2208
mrsagcan <mrsagcan@yahoo.com>
2023-02-20 02:45:31 +0300

    Creating an empty window with colors

--- main.cpp
+++ main.cpp
@@ -1,13 +1,70 @@
-#include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "main.h"
+
 
 int main()
 {
+
+	//Initializes and decides OpenGL version.
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+
+	//Creates a window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Interlinked", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//Check if GLAD loaded properly
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//Sets the location and the size of window
+	glViewport(0, 0, 800, 600);
+
+	//Call this function whenever window is resized
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	//Draw until window needs to be closed
+	while (!glfwWindowShouldClose(window))
+	{
+		//input
+		processInput(window);
+
+		//rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//swap buffers, check events
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//Clean all of GLFW's resources
+	glfwTerminate();
+
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
---
+++ main.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
