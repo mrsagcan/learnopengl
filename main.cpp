e83960701cac127317b8bdcea9515d9f1432c79d
mrsagcan <mrsagcan@yahoo.com>
2023-02-20 16:07:47 +0300

    Vertex Buffer Object and Vertex Shader

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include "main.h"
 
+const char *vertexShaderSource = 
+	"#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
 
 int main()
 {
@@ -33,6 +40,38 @@ int main()
 
 	//Call this function whenever window is resized
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	//Sending vertices to buffer
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	//Creating and compiling vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+
+	//Check whether compilation failed.
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
 
 
 	//Draw until window needs to be closed
